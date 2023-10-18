cc7f0fb6e02db517e6955582772e06fadbe18d47
Roshan <roshanneupane600@gmail.com>
2023-10-18 11:17:52 +0545

    changes

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import Image from 'next/image';
 
 
-const contact = ({data}) => {
+const contact = () => {
   return (
       <div>
         <h1>Welcome to My Page!</h1>
-        {/* <p>{data.at(1).title}</p> */}
-        {/* {data.map((element)=>{
-          return(
-            <p key={element.id}>{element.title}</p>
-
-          )
-        })} */}
       </div>
   ); 
 }
